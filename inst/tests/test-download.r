e85345923d29c01ab3dfde5c5764397ac97aa5f7
Winston Chang <winston@stdout.org>
2012-10-31 19:08:08 -0500

    Refactor tests

--- inst/tests/test-download.r
+++ inst/tests/test-download.r
@@ -1,33 +1,33 @@
 context("download")
 
-test_that("downloading https works properly", {
-  # Download https from httpbin.org
+# Download from a url, and return the contents of the file as a string
+download_result <- function(url) {
   tfile <- tempfile()
-  download("https://httpbin.org/ip", tfile, mode = "wb")
+  download(url, tfile, mode = "wb")
 
   # Read the file
   tfile_fd <- file(tfile, "r")
   dl_text <- readLines(tfile_fd, warn = FALSE)
-  dl_text <- paste(dl_text, collapse = "")
+  dl_text <- paste(dl_text, collapse = "\n")
   close(tfile_fd)
   unlink(tfile)
 
+  dl_text
+}
+
+
+test_that("downloading https works properly", {
+  # Download https from httpbin.org
+  result <- download_result("https://httpbin.org/ip")
+
   # Check that it has the string "origin" in the text
-  expect_true(grepl("origin", dl_text))
+  expect_true(grepl("origin", result))
 })
 
 test_that("follows redirects", {
-  # Download from httpbin.org
-  tfile <- tempfile()
-  download("https://httpbin.org/redirect/3", tfile, mode = "wb")
-
-  # Read the file
-  tfile_fd <- file(tfile, "r")
-  dl_text <- readLines(tfile_fd, warn = FALSE)
-  dl_text <- paste(dl_text, collapse = "")
-  close(tfile_fd)
-  unlink(tfile)
+  # Download https redirect from httpbin.org
+  result <- download_result("https://httpbin.org/redirect/3")
 
   # Check that it has the string "origin" in the text
-  expect_true(grepl("origin", dl_text))
+  expect_true(grepl("origin", result))
 })
